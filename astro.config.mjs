f685b39f853f24c15d7bda8b2830f0cff3000e79
Chris Carlin <brunscii@gmail.com>
2023-03-08 18:31:38 -0500

    changed url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig(
 	{
-		site: 'https://brunscii.github.io',
-		base: '/astro-blog-site',
+		site: 'https://brunscii.github.io/astro-blog-site',
+		// base: '/astro-blog-site/',
 		integrations: [mdx(), sitemap()],
 		markdown : {
 			syntaxHighlight: 'shiki'
