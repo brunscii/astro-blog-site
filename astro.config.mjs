b0f643fe925612e407f6e78403fdba4bbf6e2013
Chris Carlin <brunscii@gmail.com>
2023-03-08 19:08:16 -0500

    removed base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig(
 	{
 		site: 'https://brunscii.github.io/astro-blog-site/',
-		base: '/astro-blog-site/',
+		// base: '/astro-blog-site/',
 		integrations: [mdx(), sitemap()],
 		markdown : {
 			syntaxHighlight: 'shiki'
