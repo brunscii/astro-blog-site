c7eba7bf480cca503846453c24493799a6edf140
Chris Carlin <brunscii@gmail.com>
2024-06-30 04:30:59 -0400

    Changed name

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Tech Notes Blog';
+export const SITE_TITLE = 'Carlin Tech Blog';
 export const SITE_DESCRIPTION = 'Christopher Carlin\'s Blog Stite';
 export const BASE = import.meta.env.BASE_URL;
