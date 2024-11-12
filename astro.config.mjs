fcffecebb846bdf1195583c349faf45a3341e707
Chris Carlin <brunscii@gmail.com>
2024-11-12 00:07:26 -0500

    added astro node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig(
-	{
-		site: 'https://brunscii.github.io/astro-blog-site',
-		base: '/astro-blog-site',
-		publicDir: '/astro-blog-site/',
-		integrations: [mdx(), sitemap()],
-		markdown : {
-			syntaxHighlight: 'shiki'
-		}
-	}
-);
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://brunscii.github.io/astro-blog-site',
+  base: '/astro-blog-site',
+  publicDir: '/astro-blog-site/',
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    syntaxHighlight: 'shiki'
+  },
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
